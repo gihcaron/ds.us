763fa694665520ac509c19a2dae4d4395523bfc6
Giovanna Caron <158209996+gihcaron@users.noreply.github.com>
2024-11-21 15:33:42 -0300

    Feat: create Dev class

--- Segunda_Etapa/src/models/jogadores/Dev.js
+++ Segunda_Etapa/src/models/jogadores/Dev.js
@@ -1,5 +1,17 @@
+// import Aluno from './Aluno.js';
+// O aluno não está sendo importado pois não está sendo exportado no arquivo aluno.js 
+
 class Dev {
-    constructor() {
+    constructor(grupo, nome, apelido, senha, estaVivo, localAtual, mostrarPapel) {
+        this.grupo = grupo;
+        this.nome = nome;
+        this.apelido = apelido;
+        this.senha = senha;
+        this.estaVivo = estaVivo;
+        this.localAtual = localAtual;
+        this.mostrarPapel = mostrarPapel;   
     }
 
 }
+
+export default Dev;
