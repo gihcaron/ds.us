11c2e870090ae3b4f7eb45c7b3fada064a895e2a
Giovanna Caron <158209996+gihcaron@users.noreply.github.com>
2024-11-21 16:32:14 -0300

    Fix: change  Dev class

--- Segunda_Etapa/src/models/jogadores/Dev.js
+++ Segunda_Etapa/src/models/jogadores/Dev.js
@@ -1,15 +1,15 @@
 // import Aluno from './Aluno.js';
 // O aluno não está sendo importado pois não está sendo exportado no arquivo aluno.js 
 
-class Dev {
-    constructor(grupo, nome, apelido, senha, estaVivo, localAtual, mostrarPapel) {
-        this.grupo = grupo;
-        this.nome = nome;
-        this.apelido = apelido;
-        this.senha = senha;
-        this.estaVivo = estaVivo;
+class Dev extends Aluno {
+    constructor(grupo, nome, apelido, senha, estaVivo, localAtual, mostrarPapel)  {
+        super(grupo, nome, apelido, senha, estaVivo);
         this.localAtual = localAtual;
         this.mostrarPapel = mostrarPapel;   
+    }
+
+    mostrarPapel() {
+        console.log("O jogador" + this.name + "é um dev");
     }
 
 }
