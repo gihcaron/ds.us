e96026ee2e76abbfbbd838679046bd3a74155ad9
Julianeves01 <julianvs2007@gmail.com>
2024-11-21 15:58:02 -0300

    feat: create class Sabotador

--- Segunda_Etapa/src/models/jogadores/Sabotador.js
+++ Segunda_Etapa/src/models/jogadores/Sabotador.js
@@ -1,2 +1,12 @@
 class Sabotador {
+    #senha; // atributo privado
+    localAtual; // atributo público
+
+    /**
+     * @param {number} grupo // Grupo do Sabotador.
+     * @param {string} nome // Nome do Sabotador.
+     * @param {string} apelido // Apelido do sabotador.
+     * @param {string} senha // Senha do Sabotador
+     */    
 }
+
