1186d9bc31c1af9f6f5e4abad1320ead1620fd97
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-11-28 09:42:08 -0300

    feat: create get method

--- Terceira_Etapa/src/routes/cadastroRoutes.js
+++ Terceira_Etapa/src/routes/cadastroRoutes.js
@@ -7,8 +7,16 @@ cadastroRoutes.post("/adicionar", (req, res) => {
 });
 
 cadastroRoutes.get("/listar", (req, res) => {
+// Rota para listar todos os Dev's
+    return res.status(200).json(dev);
+  });
 
-});
+// Rota para listar todos os Sabotadores
+sabotadorRoutes.get("/", (req, res) => {
+    return res.status(200).json(sabotador);
+  });
+
+
 
 cadastroRoutes.put("/atualizar", (req, res) => {
 
