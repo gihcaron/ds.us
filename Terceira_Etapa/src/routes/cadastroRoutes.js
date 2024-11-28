112a54a6d12035b39f6bddb6d722881927a87ce5
Flavia Mendes <flavinharamendes@gmail.com>
2024-11-28 10:55:32 -0300

    feat: create put method

--- Terceira_Etapa/src/routes/cadastroRoutes.js
+++ Terceira_Etapa/src/routes/cadastroRoutes.js
@@ -8,57 +8,123 @@ const sabotador = [];
 
 cadastroRoutes.get("/listar/dev", (req, res) => {
   // Rota para listar todos os Dev's
-  return res.status(200).json(dev);
+return res.status(200).json(dev);
 });
 
 cadastroRoutes.get("/listar/sabotador", (req, res) => {
   // Rota para listar todos os Sabotadores
-  return res.status(200).json(sabotador);
+return res.status(200).json(sabotador);
 });
 
 cadastroRoutes.post("/adicionar/dev", (req, res) => {
-  const { nome, apelido, senha, estaVivo, localAtual, mostrarPapel } = req.body;
+const { nome, apelido, senha, estaVivo, localAtual, mostrarPapel } = req.body;
 
-  const user = {
+const user = {
     nome,
     apelido,
     senha,
     estaVivo,
     localAtual,
     mostrarPapel,
-  };
+};
 
-  dev.push(user);
+dev.push(user);
 
-  return res.status(201).json({
+return res.status(201).json({
     message: "Usuário cadastrado com sucesso",
     user,
-  });
+});
 });
 
 cadastroRoutes.post("/adicionar/sabotador", (req, res) => {
-  const { nome, apelido, senha, estaVivo, localAtual, mostrarPapel } = req.body;
+const { nome, apelido, senha, estaVivo, localAtual, mostrarPapel } = req.body;
 
-  const user = {
+const user = {
     nome,
     apelido,
     senha,
     estaVivo,
     localAtual,
     mostrarPapel,
-  };
+};
 
-  sabotador.push(user);
+sabotador.push(user);
 
-  return res.status(201).json({
+return res.status(201).json({
     message: "Usuário cadastrado com sucesso",
     user,
-  });
+});
 });
 
-cadastroRoutes.put("/atualizar", (req, res) => {
+devRoutes.put("/atualizar", (req, res) => {
 
+devRoutes.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { nome, apelido, senha, estaVivo, localAtual, mostrarPapel } = req.body;
+
+    
+    const dev = dev.find((p) => p.id == id);
+
+    if (!nome || !apelido || !senha || !estaVivo || !localAtual || !mostrarPapel) {
+    return res.status(400).json({
+        message: "Os campos nome, apelido, senha, estaVivo, localAtual e mostrarPapel são obrigatórios",
+    });
+    }
 });
+
+if (estaVivo != "sim" && estaVivo != "não") {
+    return res.status(400).send({
+    message: "Digite 'sim' ou 'não'!",
+    });
+}
+
+dev.nome = nome;
+dev.apelido = apelido;
+dev.senha = senha;
+dev.estaVivo = estaVivo || [];
+dev.localAtual = localAtual;
+dev.mostrarPapel = mostrarPapel;
+
+return res.status(200).json({
+    message: "dev atualizado com sucesso!",
+    dev,
+});
+});
+
+sabotadorRoutes.put("/atualizar", (req, res) => {
+    
+    sabotadorRoutes.put("/:id", (req, res) => {
+        const { id } = req.params;
+        const { nome, apelido, senha, estaVivo, localAtual, mostrarPapel } = req.body;
+    
+        
+        const sabotador = sabotador.find((p) => p.id == id);
+    
+        if (!nome || !apelido || !senha || !estaVivo || !localAtual || !mostrarPapel) {
+        return res.status(400).json({
+            message: "Os campos nome, apelido, senha, estaVivo, localAtual e mostrarPapel são obrigatórios",
+        });
+        }
+    });
+    
+    if (estaVivo != "sim" && estaVivo != "não") {
+        return res.status(400).send({
+        message: "Digite 'sim' ou 'não'!",
+        });
+    }
+    
+    sabotador.nome = nome;
+    sabotador.apelido = apelido;
+    sabotador.senha = senha;
+    sabotador.estaVivo = estaVivo || [];
+    sabotador.localAtual = localAtual;
+    sabotador.mostrarPapel = mostrarPapel;
+    
+    return res.status(200).json({
+        message: "sabotador atualizado com sucesso!",
+        sabotador,
+    });
+    });
 
 cadastroRoutes.delete("/remover", (req, res) => {
 
